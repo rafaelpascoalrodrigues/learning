0cde1c97286e29465405187469124807e485c543
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2018-12-11 23:51:21 -0200

    IBM_PY0101EN 01.01

---
+++ IBM_PY0101EN/module01/01_variable_types.py
@@ -0,0 +1,31 @@
+variable_int = 11
+variable_float = 22.213
+variable_string = "Hello Python 101"
+
+cast_float_to_int = int(1.1) # it's equal to 1
+cast_int_to_float = float(2)   # it's equal to 2.0
+
+cast_string_to_int = int('1') # it's equal to 1
+cast_string_to_float = float('4.5') # it's equal to 4.5
+
+cast_int_to_string = str(5) # it's equal to '5'
+cast_float_to_string = str(6.2) # it's equal to '6.2'
+
+boolean_true = True
+boolean_false = False
+
+type_of_true = type(True) # it's equal to bool
+type_of_false = type(False) # it's equal to bool
+
+cast_true_to_int = int(True) # it's equal to 1
+cast_false_to_int = int(False) # it's equal to 0
+
+cast_int_to_bool = bool(1) # it's equal to True
+cast_int_to_bool = bool(0) # it's equal to False
+
+# import library 
+import sys
+
+# show information about type
+sys.float_info
+
