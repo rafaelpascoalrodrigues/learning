10ec938cc6b38ea54a3a35c81db09a0eaa89f174
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2018-12-12 11:39:02 -0200

    IBM_PY0101EN 01.02

---
+++ IBM_PY0101EN/module01/02_operators.py
@@ -0,0 +1,27 @@
+# Mathematical Sum
+10 + 20      # it results 30
+10.2 + 20.1  # it results 30.3
+
+# Mathematical Substraction
+10 - 20     # it results -10    
+20.1 - 10.2 # it results 9.900000000000002 (???)
+
+# Mathematical Multiplication
+10 * 20     # it results 200
+10.2 * 20.1 # it results 205.02
+
+# Mathematical Division, on Pytnon 2 always returns a truncated Integer
+25 / 5 # it results 5
+25 / 6 # it results 4
+
+# Mathematical Division, on Pytnon 3 always returns Float
+25 / 5 # it results 5.0
+25 / 6 # it results 4.166666666666667
+
+# Mathematical Integer Division, always returns a truncated Integer
+25 // 5 # it results 5
+25 // 6 # it results 4
+
+# Mathematical Rest of Division
+25 % 5 # it results 0
+25 % 6 # it results 1
